05322e71fdfcb12fc713b97cb2a422ea41c5d884
Vikash <phanivikash2@gmail.com>
2025-02-20 22:56:52 +0530

    Add Header

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 from send_email import send_email
 #api_key = "ad89b54e0ff342baa73a9add458c47a5"
+topic="sports"
 url=("https://newsapi.org/v2/"
-     "everything?q=sports&from=20"
+     f"everything?q={topic}&from=20"
      "25-01-20&sortBy=publishedAt&apiKey=ad89b54e0ff342baa73a9add458c47a5&language=en")
 
 # Made a request
@@ -14,8 +15,10 @@ content=request.json()
 # Itterate over content
 body=""
 for i in content["articles"][:10]:
-    body=("Subject: Today's News "+"\n"
-          +body+str(i["title"])+"\n"+str(i["description"])+"\n"+str(i["url"])+"\n\n")
+    body=(body+str(i["title"])+"\n"+str(i["description"])+"\n"+str(i["url"])+"\n\n")
+body=("Subject: Today's News "
+      "\n"
+      +body)
 body = body.encode("utf-8")
 
 send_email(body)
